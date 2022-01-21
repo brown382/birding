99c1afa4ef3b349753b0a2fa9bf0429add578025
NEIL BROWN <neilbrown382@gmail.com>
2022-01-21 16:00:20 -0600

    updated test_1.py to match previous changes to tot_birds and freq_bird

--- test_1.py
+++ test_1.py
@@ -1,5 +1,5 @@
 from re import I
-import birds_2021
+import birds
 import pandas as pd
 import numpy as np
 import datetime as dt
@@ -19,14 +19,14 @@ def test_read_data():
     final_df_correct = pd.DataFrame(data=final_data_correct)
 
     f = r'test_read_csv.csv'
-    tested_df = birds_2021.read_data(f,date_and_time_column_name='date_and_time',date_and_time_column_positions=[3,4],idf=True,kdc=True)
+    tested_df = birds.read_data(f,date_and_time_column_name='date_and_time',date_and_time_column_positions=[3,4],idf=True,kdc=True)
     print(tested_df.dtypes)
     assert tested_df.equals(final_df_correct)
 
 def test_keep_single_column(test_dataset):
 
     final_df_correct = pd.DataFrame({'Count':[100,'X',300,400]})
-    tested_df = birds_2021.keep_columns(test_dataset,'Count')
+    tested_df = birds.keep_columns(test_dataset,'Count')
     
     assert tested_df.equals(final_df_correct)
 
@@ -35,7 +35,7 @@ def test_keep_multiple_columns(test_dataset):
     final_df_correct = pd.DataFrame({
         'Common Name':['Bob','Joe','Mary','Mary'],
         'Count':[100,'X',300,400]})
-    tested_df = birds_2021.keep_columns(test_dataset,['Common Name','Count'])
+    tested_df = birds.keep_columns(test_dataset,['Common Name','Count'])
     
     assert tested_df.equals(final_df_correct)
 
@@ -62,11 +62,11 @@ def test_rename_and_lcase_columns(test_dataset):
         'Count':'count'
     }
 
-    tested_df = birds_2021.rename_and_lcase_columns(test_dataset,test_rename_dict)
+    tested_df = birds.rename_and_lcase_columns(test_dataset,test_rename_dict)
 
     assert tested_df.equals(final_df_correct)
 
-def test_focus_on_year(test_dataset):
+def test_filter_by_dates_year(test_dataset):
 
     june_1_2020 = dt.datetime(2020,6,1)
     july_1_2020 = dt.datetime(2020,7,1)
@@ -81,11 +81,70 @@ def test_focus_on_year(test_dataset):
 
     final_df_correct = pd.DataFrame(data=d)
 
-    tested_df = birds_2021.focus_on_year(test_dataset,2020)
+    tested_df = birds.filter_by_dates(test_dataset,year=2020,start_date=None,end_date=None)
 
     assert tested_df.equals(final_df_correct)
 
-def test_drop_present_entries(test_dataset):
+def test_filter_by_dates_start_date(test_dataset):
+
+    may_1_2021 = dt.datetime(2021,5,1)
+    june_1_2020 = dt.datetime(2020,6,1)
+    july_1_2020 = dt.datetime(2020,7,1)
+
+    d = {
+        'date_and_time':[may_1_2021,june_1_2020,july_1_2020],
+        'Submission ID':[1234,2345,3456],
+        'Common Name':['Bob','Joe','Mary'],
+        'Count':[100,'X',300],
+        'Date':['2021-05-01','2020-06-01','2020-07-01'],
+        'Time':['12:32 PM','7:04 AM','10:20 AM']}
+
+    final_df_correct = pd.DataFrame(data=d)
+
+    tested_df = birds.filter_by_dates(test_dataset,start_date='2020-01-01',end_date=None,year=None)
+
+    assert tested_df.equals(final_df_correct)
+
+def test_filter_by_dates_end_date(test_dataset):
+
+    june_1_2020 = dt.datetime(2020,6,1)
+    july_1_2020 = dt.datetime(2020,7,1)
+    august_1_2019 = dt.datetime(2019,8,1)
+
+    d = {
+        'date_and_time':[june_1_2020,july_1_2020,august_1_2019],
+        'Submission ID':[2345,3456,4567],
+        'Common Name':['Joe','Mary','Mary'],
+        'Count':['X',300,400],
+        'Date':['2020-06-01','2020-07-01','2019-08-01'],
+        'Time':['7:04 AM','10:20 AM','5:15 PM']}
+
+    final_df_correct = pd.DataFrame(data=d)
+
+    tested_df = birds.filter_by_dates(test_dataset,start_date=None,end_date='2021-01-01',year=None)
+
+    assert tested_df.equals(final_df_correct)
+
+def test_filter_by_dates_start_and_end_date(test_dataset):
+
+    june_1_2020 = dt.datetime(2020,6,1)
+    july_1_2020 = dt.datetime(2020,7,1)
+
+    d = {
+        'date_and_time':[june_1_2020,july_1_2020],
+        'Submission ID':[2345,3456],
+        'Common Name':['Joe','Mary'],
+        'Count':['X',300],
+        'Date':['2020-06-01','2020-07-01'],
+        'Time':['7:04 AM','10:20 AM']}
+
+    final_df_correct = pd.DataFrame(data=d)
+
+    tested_df = birds.filter_by_dates(test_dataset,start_date='2019-12-31',end_date='2021-01-01',year=None)
+
+    assert tested_df.equals(final_df_correct)    
+
+def test_drop_val_from_col(test_dataset):
     may_1_2021 = dt.datetime(2021,5,1)
     july_1_2020 = dt.datetime(2020,7,1)
     august_1_2019 = dt.datetime(2019,8,1)
@@ -100,7 +159,7 @@ def test_drop_present_entries(test_dataset):
 
     final_correct_df = pd.DataFrame.from_dict(d)
 
-    tested_df = birds_2021.drop_val_from_col(test_dataset,'Count','X')
+    tested_df = birds.drop_val_from_col(test_dataset,'Count','X')
 
     '''Something goofy here. Tried to compare the dataframes using df.equals as 
     was done before, but assertion failed. Verified that every element, column 
@@ -109,4 +168,4 @@ def test_drop_present_entries(test_dataset):
     assert (tested_df == final_correct_df).all().all()
 
 def test_column_sum(test_dataset):
-    assert birds_2021.column_sum(test_dataset,'Submission ID') == 11602
+    assert birds.column_sum(test_dataset,'Submission ID') == 11602
